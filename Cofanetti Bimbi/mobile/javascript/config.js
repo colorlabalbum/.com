2c64e5babc76095e989621f4cd9401d39c62c43b
Colorlab <94360326+colorlabalbum@users.noreply.github.com>
2025-09-22 19:20:56 +0200

    config.js

--- Cofanetti Bimbi/mobile/javascript/config.js
+++ Cofanetti Bimbi/mobile/javascript/config.js
@@ -116,7 +116,7 @@ btnTableOfContent:["btnBookMark"]
 	pageBackgroundColor:"#FFFFFF",
 	flipshortcutbutton:"Show",
 	BookMargins:"",
-	topMargin:50,
+	topMargin:10,
 	bottomMargin:10,
 	leftMargin:10,
 	rightMargin:10,
